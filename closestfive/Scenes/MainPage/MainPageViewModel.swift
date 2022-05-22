550f69f1e0189e04f1f3f64be759987f27df6102
Abdurrahman Sanli <abdurrahman.sanli@tomdigital.com.tr>
2022-05-22 18:30:28 +0300

    Reduced result count to 5.

--- closestfive/Scenes/MainPage/MainPageViewModel.swift
+++ closestfive/Scenes/MainPage/MainPageViewModel.swift
@@ -13,6 +13,8 @@ final class MainPageViewModel {
     
     private let placesApi: PlacesApi
     
+    private let maximumItemCount = 5
+    
     init(placesApi: PlacesApi) {
         self.placesApi = placesApi
     }
@@ -24,7 +26,7 @@ final class MainPageViewModel {
     func refreshPlaces() {
         placesApi.getPlaces(latitude: 41.0086,
                             longitude: 28.8644) { placesResponse in
-            self.places = placesResponse.results
+            self.places = placesResponse.results.prefix(self.maximumItemCount).map { $0 }
             self.closurePlacesDidUpdate?()
         } failure: {
             //TODO: Handle failure
